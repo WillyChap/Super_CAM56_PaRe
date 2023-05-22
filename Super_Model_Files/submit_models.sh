f4faf21d35be1ca2300bceb9d3ce14b855a21f17
willychap <wchapman@ucsd.edu>
2023-05-22 11:39:05 -0600

    add multi-job submit

---
+++ Super_Model_Files/submit_models.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#PBS -N multi_mpi
+#PBS -A P54048000              # change to your account
+#PBS -l walltime=12:00:00
+#PBS -q regular
+#PBS -j oe
+#PBS -l select=4:ncpus=36:mpiprocs=36   # change to be total number of nodes for both jobs
+
+# Find applications to run
+DIRS=(/path/to/this/directory/CAM5_MODNAME  /path/to/this/directory/CAM6_MODNAME)
+
+# Settings
+PPA=(72 72)   # Change to be number of tasks for each job
+
+# Run applications
+LP=0
+
+for N in $(seq 0 1); do
+    cd ${DIRS[$N]}
+
+    FP=$((LP + 1))
+    LP=$((FP + ${PPA[$N]} - 1))
+
+    sed -n "$FP,$LP p" $PBS_NODEFILE > my.nodefile
+
+    echo "$(date -Is) - Starting application in ${DIRS[$N]}"
+
+    PBS_NODEFILE=my.nodefile ./case.submit --no-batch  &
+
+    # do the da type stuff
+done
+
+wait
+
+echo "$(date -Is) - All applications are finished"
